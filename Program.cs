daaf8810aafca721de8e08074a15db1dcaab462b
linzybettina <43557782+linzybettina@users.noreply.github.com>
2018-10-15 21:05:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+     static  bool done;
+
+        static void Main()
+        {
+            new Thread (Go).Start();
+            Go();
+            Console.ReadLine();
+         }
+        static void Go()
+        {
+            if (!done)
+            {
+                Console.WriteLine("Done");
+                done = true;
+            }
+
+        }
+    }
+    }
+
